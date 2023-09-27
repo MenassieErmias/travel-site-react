2852d6f8f5a8d4c6e0ec295965e6812d027ebb41
MenassieErmias <ermiasmenase@gmail.com>
2023-09-27 17:21:52 +0300

    tailwind successfully configured, clean up public folder and index.html

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black text-gray-300 flex items-center justify-center text-7xl">
+        Hello world
+      </div>
+    </>
+  );
+}
+
+export default App;
