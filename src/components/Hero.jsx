014943bfaf513bf35412698d2b1b5237cc386fc3
MenassieErmias <ermiasmenase@gmail.com>
2023-09-27 19:18:34 +0300

    responsive hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import img1 from '../assets/beach-1236581_1920.jpg';
 
+
 const Hero = () => {
+
     return (
-        <div name="home" className='bg-black text-gray-300 p-4 w-full min-h-screen h-screen'>
+        <div name="home" className='bg-black text-gray-300 p-4 w-full min-h-screen flex flex-col items-center justify-center'>
             <div className='max-w-[1000px] flex flex-col items-center justify-center w-full h-full mx-auto'>
-                <div className='grid md:grid-cols-3'>
-                    <img src={img1} alt="" className='w-full col-span-2' />
-                    <div className='grid md:grid-rows-2'>
-                        <p className='text-4xl text-bold h-full flex items-end justify-center'>
+                <div className='md:grid md:grid-cols-3 gap-3'>
+                    <img src={img1} alt="beach" className='w-full col-span-2' />
+                    <div className='md:grid md:grid-rows-2 flex flex-col items-center justify-center'>
+                        <p className='my-2 md:my-0 text-4xl text-bold h-full text-center border-b-2 border-pink-600 flex items-end justify-center'>
                             Adventure Awaits!
                         </p>
-                        <p className='text-2xl px-4 flex items-start justify-center text-center'>
-                            Explore your wildest destination yet here on Octravel!
+                        <p className='my-2 md:my-0 text-2xl h-full px-4 flex items-start justify-center text-center'>
+                            Explore your wildest destination yet, here on Octravel!
                         </p>
                     </div>
                 </div>
